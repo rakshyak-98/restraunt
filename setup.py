ff8ee1675b394ae1c4b0c09aa9a340231f9e991f
Kshitij satpathy <71308896+creaator-afk@users.noreply.github.com>
2023-12-04 18:10:17 +0530

    deployment modification

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RestRec',
+    version='0.0.0.0',
+    packages=['flask_sqlalchemy', 'bcrypt', 'numpy', 'pandas', 'seaborn', 'scikit-learn'],
+    url='',
+    license='MIT',
+    author='Arnold Anthony',
+    author_email='',
+    description='A restaurant recommendation system'
+)
