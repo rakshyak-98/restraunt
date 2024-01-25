6a8d868312823191f24fbe7872ef975249335387
st-a-anthony <133749782+st-a-anthony@users.noreply.github.com>
2024-01-25 18:35:43 +0530

    changes
    add new features

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 import main as m
-
+import main1 as m2
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
@@ -74,7 +74,7 @@ def login():
 def dashboard():
     if session['email']:
         user = User.query.filter_by(email=session['email']).first()
-        return render_template('dashboardnew.html', user=user)
+        return render_template('dashboard.html', user=user)
     
     return redirect('/login')
 
@@ -84,17 +84,86 @@ def logout():
     session.pop('email', None)
     return redirect('/login')
 
+@app.route('/location')
+def location():
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        # return render_template('locationbased.html', user=user)
+        return render_template('newlocation.html', user=user)
+    
+    return redirect('/login')
+@app.route('/rating')
+def rating():
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('rating.html', user=user)
+    
+    return redirect('/login')
+@app.route('/cuisine')
+def cuisine():
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('cuisine.html', user=user)
+    
+    return redirect('/login')
 
+@app.route('/similarrestaurent')
+def similarrestaurent():
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('similarrestaurent.html', user=user)
+    
+    return redirect('/login')
+
+@app.route('/locationsearch', methods=["GET", "POST"])
+def location_based():
+    # Get the city code from the query parameter
+    location= request.form.get('location')
+    a = m.locationbased(location)
+    # return render_template("city_hotels.html", prediction_text = "Diabetics {}".format(a))  
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('table.html', user=user, table=a, prediction_text=format(location))
+ 
 @app.route('/city', methods=["GET", "POST"])
 def city_based():
     # Get the city code from the query parameter
-    city = request.form.get('city')
+    city= request.form.get('city')
     a = m.citybased(city)
     # return render_template("city_hotels.html", prediction_text = "Diabetics {}".format(a))  
     if session['email']:
         user = User.query.filter_by(email=session['email']).first()
         return render_template('table.html', user=user, table=a, prediction_text=format(city))
-    # return render_template('dashboardnew.html', table=a)
+ 
+
+
+@app.route('/similar', methods=["GET", "POST"])
+def similar():
+
+    similar = request.form.get('similar')
+    a = m2.recommend(similar)
+
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('table2.html', user=user, table=a, prediction_text=format(similar))
+    
+
+@app.route('/rate', methods=["GET", "POST"])
+def rate():
+    rate = request.form.get('rate')
+    a = m2.predict_rating_for_restaurant(rate)
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('prediction.html', user=user,prediction_text=format(rate) , prediction_rating=format(a))
+
+@app.route('/cuisinebased', methods=["GET", "POST"])
+def cuisinebased():
+    cuisine = request.form.get('cuisine')
+    a = m.cuisinesbased(cuisine)
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('table1.html', user=user, table=a, prediction_text=format(cuisine))
+   
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -12,23 +12,15 @@ from sklearn.metrics import r2_score
 import warnings
 
 df_real=pd.read_csv(r"Dataset\zomato.csv")
-# df_real.head()
-# df_real['dish_liked'].isna()
-# df_real.isnull().sum()
+
 df=df_real.drop(['url','phone','menu_item'],axis=1)
-# df.describe()
-# df.info()
-# df.isnull().count()#gives the count of nonnull values
-# df.isnull().sum()#return the no of null values
+
 df=df.drop(['dish_liked'],axis=1)
 df.dropna(how='any',inplace=True)
-# df.info()
-# df.duplicated().sum()
+
 df.drop_duplicates(inplace=True)
 df = df.rename(columns={'approx_cost(for two people)':'cost','listed_in(type)':'type', 'listed_in(city)':'city'})
-# df.head()
-# df['votes'].isna().sum()
-# df['cost']
+
 df['cost']=df['cost'].astype(str)
 df['cost']=df['cost'].apply(lambda x:x.replace(',','.'))
 df['cost']=df['cost'].astype(float)
@@ -48,24 +40,8 @@ df['rate'] =df['rate'].apply(remove_slash).astype(float)
 df.name=df.name.apply(lambda x:x.title())
 df.online_order.replace(('Yes','No'),(True,False),inplace=True)
 df.book_table.replace(('Yes','NO'),(True,False),inplace=True)
-# df['online_order']
-# df['book_table']
-# df.isnull().sum()#return the no of null values
-# df.describe()
-# df.info()
-# df['rate'].count()#Total no of rating values in the DataFrame
-# df['rate'].unique()#Total no of unique rating value in the DataFrame
-# df['rate'].unique().sum()
-# df.head()
-# df['type'].count()
-# df['type'].unique()
-# df['reviews_list']
-# rs_name=list(df['name'].unique())
-# rs_name
-# df.columns
-# df.columns
-# df.city.unique()
-def citybased(city):
+
+def locationbased(city):
     df['city']=df['city'].str.lower()
     citybase=df[df['city']==city.lower()]
     citybase=citybase.sort_values(by='rate',ascending=False)
@@ -78,3 +54,42 @@ def citybased(city):
     else:
         print('No Hotels Available')
 
+def cuisinesbased(cuisines):
+    df['cuisines']=df['cuisines'].str.lower()
+    cuisinesbase=df[df['cuisines']==cuisines.lower()]
+    cuisinesbase=cuisinesbase.sort_values(by='rate',ascending=False)
+    cuisinesbase.drop_duplicates(subset='address',keep='first',inplace=True)
+    if(cuisinesbase.empty==0):
+        hname=cuisinesbase[['address', 'name', 'cuisines','online_order', 'book_table', 'rate']]
+        e=pd.DataFrame( hname.head(10))
+        html_table = e.to_html(classes='table table-bordered', index=False)
+        return (html_table)
+    else:
+        print('No Hotels Available')
+
+# print(cuisinesbased("cafe"))
+        
+data=pd.read_csv(r"C:\Users\ASUSU\PycharmProjects\Zomato\zomato_restaurants_in_India.csv")
+selected_columns = ['name', 'address', 'city', 'cuisines', 'aggregate_rating', 'delivery', 'takeaway']
+new_df = data[selected_columns]
+new_df['delivery'] = new_df['delivery'].replace({-1: 'No', 1: 'Yes'})
+new_df['takeaway'] = new_df['takeaway'].replace({-1: 'No', 1: 'Yes'})
+new_df.drop_duplicates(inplace=True)
+new_df.dropna(how='any',inplace=True)
+new_df = new_df.rename(columns={'name':'Name','address':'Address', 'city':'City','cuisines':'Cuisines','aggregate_rating':'Rating','delivery':'Delivery','takeaway':'Takeaway'})
+def citybased(city):
+    new_df['City']=new_df['City'].str.lower()
+    citybase=new_df[new_df['City']==city.lower()]
+    citybase=citybase.sort_values(by='Rating',ascending=False)
+    citybase.drop_duplicates(subset='Address',keep='first',inplace=True)
+    if(citybase.empty==0):
+        hname=citybase[[ 'Name', 'Address','City', 'Cuisines', 'Rating', 'Delivery',
+       'Takeaway']]
+        e = pd.DataFrame(hname.head(10))
+        html_table = e.to_html(classes='table table-bordered', index=False)
+        return (html_table)
+    else:
+        print('No Hotels Available')  
+   
+
+# print(citybased('agra'))
